ac75061dc41106f64f6033656105def8de85b746
Ahmed140298 <116595107+Ahmed140298@users.noreply.github.com>
2023-02-27 16:36:58 +0000

    vixed the failed tests

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -7,7 +7,7 @@ class Engineer extends Employee {
     this.github = github;
   }
   getGithub(){
-    return `GitHubUser {this.github}`
+    return this.github
   }
   getRole(){
     return 'Engineer';
